8bea4ca8a8e21e456b82a8ad34211c8c5aff14be
Vedavrat <vedavratpp@gmail.com>
2023-06-18 19:02:40 +0530

    fix: add categories route

--- src/model/mainCategory.model.ts
+++ src/model/mainCategory.model.ts
@@ -1,15 +1,13 @@
 import { Schema, model, Document, PopulatedDoc } from 'mongoose';
 import { IChildCategory } from './childCategory.mode';
-import { IVariant } from './variant.model';
 import { IRootCategory } from './rootCategory.model';
 
 interface IMainCategory extends Document {
   name: string;
   description?: string;
   slug: string;
-  parentCategoryId: PopulatedDoc<Schema.Types.ObjectId & IRootCategory>;
+  parentCategoryIds: PopulatedDoc<Schema.Types.ObjectId & IRootCategory>[];
   children: IChildCategory[];
-  variants: PopulatedDoc<Schema.Types.ObjectId & IVariant>[];
 }
 
 const categorySchema = new Schema<IMainCategory>(
@@ -17,9 +15,8 @@ const categorySchema = new Schema<IMainCategory>(
     name: { type: String, required: true },
     description: { type: String },
     slug: { type: String, required: true },
-    parentCategoryId: { type: Schema.Types.ObjectId, ref: 'RootCategory' },
+    parentCategoryIds: [{ type: Schema.Types.ObjectId, ref: 'RootCategory' }],
     children: [{ type: Schema.Types.ObjectId, ref: 'ChildCategory' }],
-    variants: [{ type: Schema.Types.ObjectId, ref: 'Variant' }],
   },
   { timestamps: true, versionKey: false },
 );
