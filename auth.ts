0112e6ebd9600ed4bd067d5937f1261a9f507819
Nishkb <66142773+Nishkb@users.noreply.github.com>
2024-02-14 23:19:38 -0500

    Add files via upload

---
+++ auth.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import { validateToken } from '@src/services/auth.service';
+import { BadRequestError, AuthFailureError } from '@src/utils/apiError';
+
+const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Response<unknown, Record<string, unknown>> | void => {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+  if (!token) {
+    throw new BadRequestError(`Token is missing`);
+  }
+
+  try {
+    const decoded = validateToken(token); 
+    req.body.decoded = decoded;
+    return next();
+  } catch (err) {
+    // throw new AuthFailureError(`Unauthorized`);
+    return res.redirect('/login');    // Added by Prashant
+  }
+};
+
+export default authMiddleware;
